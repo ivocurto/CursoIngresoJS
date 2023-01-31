056b2191380f958a39ab97968c11da5922e7b6b5
Ivo <ivo.curto7@gmail.com>
2023-01-31 09:46:19 -0300

    Se modificó un ejercicio

--- 1-EntradaSalida/jsEntradaSalida-01.js
+++ 1-EntradaSalida/jsEntradaSalida-01.js
@@ -1,8 +1,8 @@
-/*
+/*Curto Eivers Ivo
 Al presionar el  botón, 
 se debe mostrar un mensaje como el siguiente "Esto funciona de maravilla"*/
 function mostrar()
 {
-	alert("Hola Mundo");
+	alert("Hola");
 }
 
