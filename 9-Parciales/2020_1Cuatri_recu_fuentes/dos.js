9f6826db4a4efd8c746b9a74a1ae70b9888616d9
Ivo <ivo.curto7@gmail.com>
2023-02-27 19:34:28 -0300

    Corregir recu parcial 2020 2

--- 9-Parciales/2020_1Cuatri_recu_fuentes/dos.js
+++ 9-Parciales/2020_1Cuatri_recu_fuentes/dos.js
@@ -2,15 +2,20 @@
 
 Realizar el algoritmo que permita ingresar los datos de una compra de productos de la construccion, hasta que el cliente quiera:
 Tipo validad("arena";"cal";"cemento")
-Cantidad de bolsas,
-Precio por bolsa (más de cero ),
+Cantidad de bolsas(0-300),
+Precio por bolsa (más de cero y menor a 10000 ),
 al terminar la compra el cliente accede a un descuento segun las bolsas en total
-Si compro más de 10 bolsas en total tenes 15% de descuento sobre el total a pagar.
-Si compro más de 30 bolsas en total tenes 25% de descuento sobre el total a pagar.
-a) El importe total a pagar , bruto sin descuento y...
-b) el importe total a pagar con descuento(solo si corresponde)
-d) Informar el tipo ("arena";"cal";"cemento") con mas cantidad de bolsas en el total de la compra.
-f) El tipo mas caro */
+Si compro más de 15 bolsas en total tenes 10% de descuento sobre el total a pagar.
+Si compro más de 45 bolsas en total tenes 30% de descuento sobre el total a pagar.
+a)
+El importe total a pagar , bruto sin descuento y...
+b)
+el importe total a pagar con descuento(solo si corresponde)
+d)
+Informar la bolsa mas cara de Cal y la mas barata de Cemento.
+e)
+Cuantas bolsas de arena se compraron en total, y el promedio por compra. 
+ */
 function mostrar()
 {
   let productoIngresado;
@@ -21,19 +26,26 @@ function mostrar()
   let porcentaje;
   let precioTotal;
   let precioTotalConDescuento;
-  let banderaTipoMayorCantidad;
-  let tipoMayorCantidad;
-  let mayorCantidadBolsas;
+  let bolsaMasCaraDeCal;
+  let banderaBolsaMasCaraDeCal;
+  let bolsaMasCaraDeCemento;
+  let banderaBolsaMasCaraDeCemento;
+  let contadorBolsasArena;
+  let precioTotalBolsasArena;
+  let promedioCompraArena;
 
   respuesta = "si"
   porcentaje = 0;
-  banderaTipoMayorCantidad = true;
   cantidadDeBolsas = 0;
   precioPorBolsa = 0;
-  mayorCantidadBolsas = 0;
   precioTotal = 0;
   cantidadBolsasTotales = 0;
   precioTotalConDescuento = 0;
+  banderaBolsaMasCaraDeCal = true;
+  banderaBolsaMasCaraDeCemento = true;
+  bolsaMasCaraDeCal = 0;
+  bolsaMasCaraDeCemento = 0;
+  contadorBolsasArena = 0;
 
   while (respuesta == "si") {
     productoIngresado = prompt('Ingrese el producto deseado: "arena", "cal" o "cemento"');
@@ -41,23 +53,39 @@ function mostrar()
       productoIngresado = prompt('Error. Ingrese el producto deseado: "arena", "cal" o "cemento"');
     }
 
-    cantidadDeBolsas = parseInt(prompt('Ingrese la cantidad de bolsas que desea'));
-    while (isNaN(cantidadDeBolsas) || cantidadDeBolsas < 1) {
-      cantidadDeBolsas = parseInt(prompt('Error. Ingrese un número válido'));
+    cantidadDeBolsas = parseInt(prompt('Ingrese la cantidad de bolsas que desea (máximo 300).'));
+    while (isNaN(cantidadDeBolsas) || cantidadDeBolsas < 1 || cantidadDeBolsas > 300) {
+      cantidadDeBolsas = parseInt(prompt('Error. Ingrese un número válido (máximo 300).'));
     }
 
     precioPorBolsa = parseInt(prompt('Ingrese el precio por bolsa'));
-    while (isNaN(precioPorBolsa) || precioPorBolsa < 1) {
+    while (isNaN(precioPorBolsa) || precioPorBolsa < 1 || precioPorBolsa > 10000) {
       precioPorBolsa = parseInt(prompt('Error. Ingrese un número válido'));
     }
 
-    if (banderaTipoMayorCantidad == true) {
-      tipoMayorCantidad = productoIngresado;
-      mayorCantidadBolsas = cantidadDeBolsas;
-      banderaTipoMayorCantidad = false;
-    } else if (mayorCantidadBolsas < cantidadDeBolsas) {
-      tipoMayorCantidad = productoIngresado;
-      mayorCantidadBolsas = cantidadDeBolsas;
+    //d) Informar la bolsa mas cara de Cal y la mas barata de Cemento.
+    if (productoIngresado == "cal") {
+      if (banderaBolsaMasCaraDeCal == true) {
+        bolsaMasCaraDeCal = precioPorBolsa;
+        banderaBolsaMasCaraDeCal = false;
+      } else if (bolsaMasCaraDeCal < precioPorBolsa) {
+        bolsaMasCaraDeCal = precioPorBolsa
+      }
+    }
+
+    if (productoIngresado == "cemento") {
+      if (banderaBolsaMasCaraDeCemento == true) {
+        bolsaMasCaraDeCemento = precioPorBolsa;
+        banderaBolsaMasCaraDeCemento = false;
+      } else if (bolsaMasCaraDeCemento < precioPorBolsa) {
+        bolsaMasCaraDeCemento = precioPorBolsa
+      }
+    }
+
+    //e)Cuantas bolsas de arena se compraron en total, y el promedio por compra.
+    if (productoIngresado == "arena") {
+      contadorBolsasArena = contadorBolsasArena + cantidadDeBolsas;
+      precioTotalBolsasArena = precioTotalBolsasArena + precioPorBolsa * cantidadDeBolsas
     }
 
     cantidadBolsasTotales = cantidadBolsasTotales + cantidadDeBolsas;
@@ -66,15 +94,17 @@ function mostrar()
     respuesta = prompt("¿Desea seguir ingresando productos? 'si/no'");
   }
 
-  if (cantidadBolsasTotales > 10) {
-    porcentaje = -15;
-  } else if (cantidadBolsasTotales > 30) {
+  if (cantidadBolsasTotales > 15) {
+    porcentaje = -10;
+  } else if (cantidadBolsasTotales > 45) {
     porcentaje = -30;
   }
 
   precioTotalConDescuento = precioTotal + (precioTotal * porcentaje / 100);
+  promedioCompraArena = precioTotalBolsasArena / contadorBolsasArena;
 
   document.write("El importe total a pagar, bruto sin descuento es de " + precioTotal + "<br>");
   document.write("El importe total a pagar con descuento es de " + precioTotalConDescuento + "<br>");
-  document.write("El tipo con mas cantidad de bolsas es " + tipoMayorCantidad + "<br>");
+  document.write("La bolsa mas cara de cal cuesta $" + bolsaMasCaraDeCal + "<br>");
+  document.write("La bolsa mas cara de cemento cuesta $" + bolsaMasCaraDeCemento + "<br>");
 }
